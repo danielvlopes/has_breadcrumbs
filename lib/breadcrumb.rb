493b7b6990415d5c76a4568beda71cb7d11808a5
Daniel Lopes <danielvlopes@gmail.com>
2009-07-17 20:52:12 -0300

    add css example, fix readme

--- lib/breadcrumb.rb
+++ lib/breadcrumb.rb
@@ -14,25 +14,22 @@ class Breadcrumb
   def display
     size = @items.size
 
-    if size > 1
-      crumbs = @items.to_enum(:each_with_index).collect do |item, index|
-        name, url, options = item
+    crumbs = @items.to_enum(:each_with_index).collect do |item, index|
+      name, url, options = item
 
-        options[:class] ||= ""
-        options[:class] << " item-#{index}"
-        options[:class] << " last" if size - 1 == index
-        options[:class].squish!
+      options[:class] ||= ""
+      options[:class] << " item-#{index}"
+      options[:class] << " last" if size - 1 == index
+      options[:class].squish!
 
-        if url.nil? || (size - 1 == index)
-          content_tag(:li, name, options)
-        else
-          content_tag(:li, content_tag(:a, name, :href => url), options)
-        end
-      end.join("\n")
+      if url.nil? || (size - 1 == index)
+        content_tag(:li, name, options)
+      else
+        content_tag(:li, content_tag(:a, name, :href => url), options)
+      end
+    end.join("\n")
 
-      content_tag(:ul, crumbs, :id=>"breadcrumbs")
-    end
-
+    content_tag(:ul, "\n#{crumbs}\n", :id=>"breadcrumbs")
   end
 
 end
